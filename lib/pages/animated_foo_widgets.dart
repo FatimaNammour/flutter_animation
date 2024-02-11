9d694c65027c8630bb19727a6533f1a441942044
FatimaNamm9 <nammouraftima@gmail.com>
2024-02-11 22:43:57 +0300

    fix overflow

--- lib/pages/animated_foo_widgets.dart
+++ lib/pages/animated_foo_widgets.dart
@@ -17,144 +17,162 @@ class HomeScreenState extends State<HomeScreen> {
   String imageUrl = imagesUrls[0];
   Alignment alignment = Alignment.bottomLeft;
   double _turns = 0.0;
+  double _opacity = 1.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const SharedAppBar(title: "Animated Foo Widgets"),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Wrap(
-            children: [
-              Column(
-                children: [
-                  AnimatedContainer(
-                    curve: Curves.elasticOut,
-                    duration: const Duration(seconds: 2),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10))),
-                    width: dimension,
-                    height: dimension,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (dimension == 100) {
-                            dimension = 200;
-                          } else {
-                            dimension = 100;
-                          }
-                        });
-                      },
-                      child: const Text("Scale container")),
-                ],
-              ),
-              Column(
-                children: [
-                  AnimatedContainer(
-                    duration: const Duration(seconds: 1),
-                    decoration: BoxDecoration(
-                        color: _conColor,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10))),
-                    width: 100,
-                    height: 100,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (_conColor == Colors.purple) {
-                            _conColor = Colors.green;
-                          } else {
-                            _conColor = Colors.purple;
-                          }
-                        });
-                      },
-                      child: const Text("Change color")),
-                ],
-              ),
-              Column(
-                children: [
-                  AnimatedContainer(
-                    duration: const Duration(seconds: 1),
-                    decoration: BoxDecoration(
-                        image: DecorationImage(image: NetworkImage(imageUrl)),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10))),
-                    width: 200,
-                    height: 200,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (imageUrl == imagesUrls[0]) {
-                            imageUrl = imagesUrls[1];
-                          } else {
-                            imageUrl = imagesUrls[0];
-                          }
-                        });
-                      },
-                      child: const Text("Change image")),
-                ],
-              ),
-              Column(
-                children: [
-                  Container(
-                    color: Theme.of(context).primaryColor,
-                    height: 100,
-                    width: 150,
-                    child: AnimatedAlign(
-                      alignment: alignment,
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Wrap(
+              children: [
+                Column(
+                  children: [
+                    AnimatedContainer(
+                      curve: Curves.elasticOut,
                       duration: const Duration(seconds: 2),
-                      child: const Text("kjfhgkjfdb",
-                          style: TextStyle(color: Colors.white)),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10))),
+                      width: dimension,
+                      height: dimension,
                     ),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (alignment == Alignment.bottomLeft) {
-                            alignment = Alignment.topRight;
-                          } else {
-                            alignment = Alignment.bottomLeft;
-                          }
-                        });
-                      },
-                      child: const Text("Change align")),
-                ],
-              ),
-              Column(
-                children: [
-                  AnimatedRotation(
-                    turns: _turns,
-                    duration: const Duration(seconds: 2),
-                    child: const FlutterLogo(size: 100),
-                  ),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {});
-                          },
-                          child: const Text("Rotate Right")),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              _turns = _turns - 0.5;
-                            });
-                          },
-                          child: const Text("Rotate left")),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (dimension == 100) {
+                              dimension = 200;
+                            } else {
+                              dimension = 100;
+                            }
+                          });
+                        },
+                        child: const Text("Scale container")),
+                  ],
+                ),
+                Column(
+                  children: [
+                    AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      decoration: BoxDecoration(
+                          color: _conColor,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10))),
+                      width: 100,
+                      height: 100,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_conColor == Colors.purple) {
+                              _conColor = Colors.green;
+                            } else {
+                              _conColor = Colors.purple;
+                            }
+                          });
+                        },
+                        child: const Text("Change color")),
+                  ],
+                ),
+                Column(
+                  children: [
+                    AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage(imageUrl)),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10))),
+                      width: 200,
+                      height: 200,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (imageUrl == imagesUrls[0]) {
+                              imageUrl = imagesUrls[1];
+                            } else {
+                              imageUrl = imagesUrls[0];
+                            }
+                          });
+                        },
+                        child: const Text("Change image")),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      color: Theme.of(context).primaryColor,
+                      height: 100,
+                      width: 150,
+                      child: AnimatedAlign(
+                        alignment: alignment,
+                        duration: const Duration(seconds: 2),
+                        child: const Text("kjfhgkjfdb",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (alignment == Alignment.bottomLeft) {
+                              alignment = Alignment.topRight;
+                            } else {
+                              alignment = Alignment.bottomLeft;
+                            }
+                          });
+                        },
+                        child: const Text("Change align")),
+                  ],
+                ),
+                Column(
+                  children: [
+                    AnimatedRotation(
+                      turns: _turns,
+                      duration: const Duration(seconds: 2),
+                      child: const FlutterLogo(size: 100),
+                    ),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _turns = _turns - 0.5;
+                              });
+                            },
+                            child: const Text("Rotate left")),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    AnimatedOpacity(
+                      opacity: _opacity,
+                      duration: const Duration(seconds: 2),
+                      child: const FlutterLogo(size: 100),
+                    ),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _opacity = _opacity == 1.0 ? 0.0 : 1.0;
+                              });
+                            },
+                            child: const Text("Animate Opacity")),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       )),
     );
   }
